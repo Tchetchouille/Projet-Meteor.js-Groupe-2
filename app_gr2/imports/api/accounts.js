b1179c9ec87b78c28eb38e67b150d6dee05f9e40
MennaGe <menna.genoud@unil.ch>
2018-04-11 11:06:30 +0200

    Mise à jour profil

--- app_gr2/imports/api/accounts.js
+++ app_gr2/imports/api/accounts.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
-import import './formulaire_profil.html';
+import import '../ui/formulaire_profil.html';
 
 Template.formulaire_profil.events({
 'submit form': function(){
@@ -10,17 +10,11 @@ Template.formulaire_profil.events({
 nom = $('#Nom').val(),
 prenom = $('#Prénom').val(),
 universite = $('#Université').val(),
-password = $('#password').val(),
-phone = $('phone').val();
 
 // Ensuite :
 Accounts.createUser({
-    email: email,
-    firstname: firstName,
-    lastname: lastName,
-    institute: institute,
-    password: password,
-    phone: phone,
-    sw : true
+    Nom: nom,
+    Prenom: prenom,
+    Université: universite,
 }
 });
