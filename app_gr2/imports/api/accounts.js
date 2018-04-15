64745cd9a4b6f99eed1310ad183e4452d5d58a95
Tchetchouille <Tchetchouille@users.noreply.github.com>
2018-04-15 18:29:16 +0200

    Premières lignes - Brouillon
    J'ai commencé à écrire un peu mais ça reste du brouillon. J'ai pas eu le temps de faire qqch de fonctionnel, il faudra que je complète. En plus je me suis rendu compte que je comprends pas ce que Menna a commencé donc j'ai crée des choses assez similaires pour pouvoir aller plus loin. il faudra qu'on "merge" tout ça.

--- app_gr2/imports/api/accounts.js
+++ app_gr2/imports/api/accounts.js
@@ -3,18 +3,21 @@ import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 import '../ui/formulaire_profil.html';
 
+
+
 Template.formulaire_profil.events({
-'submit form': function(){
-// Première base : Créer les variables 
+    'submit form': function(){
+        // Première base : Créer les variables 
 
-nom = $('#Nom').val(),
-prenom = $('#Prénom').val(),
-universite = $('#Université').val(),
+        nom = $('#Nom').val(),
+        prenom = $('#Prénom').val(),
+        universite = $('#Université').val(),
 
-// Ensuite :
-Accounts.createUser({
-    Nom: nom,
-    Prenom: prenom,
-    Université: universite,
-}
+        // Ensuite :
+        Accounts.createUser({
+            Nom: nom,
+            Prenom: prenom,
+            Université: universite
+        });
+    }
 });
