a630e0ce4961e47dc02d81211713fa802c8785c1
Tchetchouille <Tchetchouille@users.noreply.github.com>
2018-04-18 10:25:12 +0200

    Rien d'important

--- app_gr2/client/main.js
+++ app_gr2/client/main.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../imports/ui/body.js'
 
 //Je met ça là pour l'instant
 Template.formulaire_inscription_profil.helpers({
