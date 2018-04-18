57fbd109c2f1991a5c9cb02bc99edb41e02266da
Tchetchouille <Tchetchouille@users.noreply.github.com>
2018-04-18 09:33:33 +0200

    2-3 modif sur formulaire_inscription_profil

--- app_gr2/client/main.js
+++ app_gr2/client/main.js
@@ -5,6 +5,7 @@ import './main.html';
 
 //Je met ça là pour l'instant
 Template.formulaire_inscription_profil.helpers({
+    //signingIn permet d'afficher des choses différentes en fonction de si la personne remplit le formulaire d'inscription ou non.
     'signingIn' : function(){
         let signingIn = Session.get('signingIn');
         return signingIn;
@@ -17,7 +18,7 @@ Template.formulaire_inscription_profil.events({
         Session.set('signingIn', true);
     },
     //En cliquant ce bouton, on affiche(ra) le login
-    'click .logIn' : function(){
+    'click .retour' : function(){
         Session.set('signingIn', false);
     },
     'submit form' : function(event){
