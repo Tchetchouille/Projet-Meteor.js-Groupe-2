ddb403e8664a2aa26f994c39c333bd9dd818b114
Tchetchouille <Tchetchouille@users.noreply.github.com>
2018-04-18 10:54:01 +0200

    reglé conflit ?

--- app_gr2/imports/ui/body.js
+++ app_gr2/imports/ui/body.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-=======
->>>>>>> 45d278643a62498ac03864cc435d91b491bd50ba
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
@@ -20,7 +14,6 @@ Template.formulaire_inscription_profil.helpers({
     }
 });
 
-<<<<<<< HEAD
 Template.formulaire_inscription_profil.events({
     //En cliquant ce bouton, on affiche le formulaire
     'click .signIn' : function(){
@@ -36,7 +29,4 @@ Template.formulaire_inscription_profil.events({
         //Il faudra que je crée la méthode pour ajouter un document dans la collection
     }
 });
-=======
->>>>>>> a630e0ce4961e47dc02d81211713fa802c8785c1
-import './formulaire_profil.html';
->>>>>>> 45d278643a62498ac03864cc435d91b491bd50ba
+
