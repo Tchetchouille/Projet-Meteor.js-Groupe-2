0a8658b7788137966b3c288f8275ac8afaafdcdb
MennaGe <menna.genoud@unil.ch>
2018-04-18 10:47:57 +0200

    test

--- app_gr2/imports/ui/body.js
+++ app_gr2/imports/ui/body.js
@@ -1,2 +1,2 @@
 
-import './formulaire_modification_profil.html';
+import './formulaire_profil.html';
--- app_gr2/server/main.js
+++ app_gr2/server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/ui/body.js'
+import './main.html'
 
 Meteor.startup(() => {
   // code to run on server at startup
