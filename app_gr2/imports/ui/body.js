6866c9dbcf31c1ad738a73c82d413ab497ce8745
Tchetchouille <Tchetchouille@users.noreply.github.com>
2018-04-22 13:16:37 +0200

    Quelques modification pour le template logIn

--- app_gr2/imports/ui/body.js
+++ app_gr2/imports/ui/body.js
@@ -4,11 +4,12 @@ import { Accounts } from 'meteor/accounts-base';
 
 import './body.html';
 import './templates/formulaire_profil.html';
+import './templates/logIn.html';
 
 
 //INSCRIPTION 
 
-Template.formulaire_inscription_profil.helpers({
+Template.logIn.helpers({
     //signingIn permet d'afficher des choses différentes en fonction de si la personne remplit le formulaire d'inscription ou non.
     'signingIn' : function(){
         let signingIn = Session.get('signingIn');
@@ -18,9 +19,10 @@ Template.formulaire_inscription_profil.helpers({
         let logedIn = Session.get('logedIn');
         return logedIn;
     },
+    
 });
 
-Template.formulaire_inscription_profil.events({
+Template.logIn.events({
     //En cliquant ce bouton, on affiche le formulaire
     'click .signIn' : function(){
         Session.set('signingIn', true);
@@ -28,7 +30,11 @@ Template.formulaire_inscription_profil.events({
     //En cliquant ce bouton, le formulaire disparait.
     'click .retour' : function(){
         Session.set('signingIn', false);
-    },
+    }
+});
+
+
+Template.formulaire_inscription_profil.events({
     'submit .formulaire_inscription_profil' : function(event){
         //Pour ne pas recharger la page
         event.preventDefault();
@@ -44,6 +50,9 @@ Template.formulaire_inscription_profil.events({
         Accounts.createUser({email: $('[id="email_adress"]').val(),password: $('[id="password"]').val()});
         //La méthode qui créera le profil lié à l'utilisateur
         Meteor.call('creerUtilisateur', newUserData);
+        
+        //L'utilisateur est automatiquement loged in quand il crée son compte.
+        Session.set('logedIn', true);
+        Session.set('signingIn', false);
     }
 });
-
--- app_gr2/methods.js
+++ app_gr2/methods.js
@@ -1,23 +1,18 @@
-/*
-Après la création de l'utilisateur, ajoute les autres champs d'information
-Problème 1 : Il y a un délai lors de la création de l'utilisateur. La méthode se lance avant qu'il ne soit créé. Donc findOne retourne undefined.
-Problème 2 : Actuellement, un changement est opéré mais ça "flash". Il apparait et disparait une fraction de seconde plus tard...
-*/
+
 Meteor.methods({
+    //Je remplis une autre collection puisque c'est visiblement impossible avec users.
     'creerUtilisateur': function(newUserData){
         //Pour pouvoir utiliser l'_id de l'utilisateur comme foreign key
-        let userId = Meteor.users.findOne({"emails.address": 'newUserData.email'}, {_id: 1});
-
+        let userId = Meteor.users.find({"emails.address": 'test@test.test'}).fetch();
+        console.log(userId)
+        
         ProfilesUtilisateurs.insert({
             userId: userId,
             nom: newUserData.nom,
             prenom: newUserData.prenom,
             email: newUserData.email,
-            mot_de_passe: newUserData.mot_de_passe,
             universite: newUserData.universite,
             domaine: newUserData.domaine
         });
-        //Pour faire disparaître le template d'inscription une fois l'inscription faite.
-        Session.set('logedIn', true);
     },
   });
