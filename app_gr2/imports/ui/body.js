33b5f626c4e52d63a7a747b0489f5164620040bb
Tchetchouille <Tchetchouille@users.noreply.github.com>
2018-04-22 15:09:50 +0200

    "Terminé" l'inscription, commencé le login

--- app_gr2/imports/ui/body.js
+++ app_gr2/imports/ui/body.js
@@ -5,9 +5,8 @@ import { Accounts } from 'meteor/accounts-base';
 import './body.html';
 import './templates/formulaire_profil.html';
 import './templates/logIn.html';
+import './templates/barre_navigation.html';
 
-
-//INSCRIPTION 
 
 Template.logIn.helpers({
     //signingIn permet d'afficher des choses différentes en fonction de si la personne remplit le formulaire d'inscription ou non.
@@ -15,11 +14,14 @@ Template.logIn.helpers({
         let signingIn = Session.get('signingIn');
         return signingIn;
     },
+    'logingIn' : function(){
+        let logingIn = Session.get('logingIn');
+        return logingIn;
+    },
     'logedIn' : function(){
         let logedIn = Session.get('logedIn');
         return logedIn;
-    },
-    
+    }    
 });
 
 Template.logIn.events({
@@ -27,32 +29,64 @@ Template.logIn.events({
     'click .signIn' : function(){
         Session.set('signingIn', true);
     },
+
+    //WORK IN PROGRESS : Je dois me renseigner sur le login du package
+    'submit .formLogIn' : function(event){
+        event.preventDefault();
+        let emailVar = event.target.loginEmail.value;
+        let passwordVar = event.target.loginPassword.value;
+        Meteor.loginWithPassword(emailVar, passwordVar);
+        Session.set('logedIn', true);
+    },
     //En cliquant ce bouton, le formulaire disparait.
     'click .retour' : function(){
         Session.set('signingIn', false);
     }
 });
 
+Template.barre_navigation.helpers({
+    'logedIn' : function(){
+        let logedIn = Session.get('logedIn');
+        return logedIn;
+    }
+});
+
+Template.barre_navigation.events({
+    'click .logOut' : function(){
+        Session.set('logedIn', false);
+    }
+});
 
 Template.formulaire_inscription_profil.events({
     'submit .formulaire_inscription_profil' : function(event){
         //Pour ne pas recharger la page
         event.preventDefault();
-        //Ici j'essaie de stocker toutes ces infos dans newUserData, de la même manière que "adress" et "verified" sont stockés dans "email"
-        let newUserData = {
-            nom: $('[id=name]').val(),
-            prenom: $('[id=firstName]').val(),
-            email:  $('[id=email_adress]').val(),
-            mot_de_passe:  $('[id=password]').val(),
-            universite:  $('[id=university]').val(),
-            domaine:  $('[id=branch]').val()
-        };
-        Accounts.createUser({email: $('[id="email_adress"]').val(),password: $('[id="password"]').val()});
-        //La méthode qui créera le profil lié à l'utilisateur
-        Meteor.call('creerUtilisateur', newUserData);
 
-        //L'utilisateur est automatiquement loged in quand il crée son compte.
-        Session.set('logedIn', true);
-        Session.set('signingIn', false);
+        //Pour vérifier que le mdp est bien entré correctement
+        let mdp = $('[id=password]').val();
+        let mdpControle = $('[id=password_verification]').val();
+
+        if(mdp==mdpControle){
+            //Ici j'essaie de stocker toutes ces infos dans newUserData, de la même manière que "adress" et "verified" sont stockés dans "email"
+            let newUserData = {
+                nom: $('[id=name]').val(),
+                prenom: $('[id=firstName]').val(),
+                email:  $('[id=email_adress]').val(),
+                mot_de_passe:  mdp,
+                universite:  $('[id=university]').val(),
+                domaine:  $('[id=branch]').val()
+            };
+            Accounts.createUser({email: $('[id="email_adress"]').val(),password: $('[id="password"]').val()});
+            //La méthode qui créera le profil lié à l'utilisateur
+            Meteor.call('creerUtilisateur', newUserData);
+
+            //L'utilisateur est automatiquement loged in quand il crée son compte.
+            Session.set('logedIn', true);
+            Session.set('signingIn', false);
+        }
+        else{
+            //Dans un second temps il faudra prendre en compte tous les champs et afficher les remarques directement sur les champs qui posent problèmes
+            alert("Le mot de passe ne correspond pas à la vérification !")
+        }
     }
 });
--- app_gr2/methods.js
+++ app_gr2/methods.js
@@ -3,12 +3,10 @@ Meteor.methods({
     //Je remplis une autre collection puisque c'est visiblement impossible avec users.
     'creerUtilisateur': function(newUserData){
         //Pour pouvoir utiliser l'_id de l'utilisateur comme foreign key
-        let userId = Meteor.users.find({'emails.address': 'test@test.com'}).fetch();
+        let currentUserId = Meteor.users.find({'emails.address': 'test@test.com'}).fetch();
 
-        console.log(userId);
-        
         ProfilesUtilisateurs.insert({
-            userId: userId[0]._id,
+            userId: currentUserId[0]._id,
             nom: newUserData.nom,
             prenom: newUserData.prenom,
             email: newUserData.email,
